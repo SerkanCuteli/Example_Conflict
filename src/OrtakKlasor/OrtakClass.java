1d0930c6db028ce3a6f1bd1ec0324821b7f81ea3
SerkanCuteli <serkancuteli@gmail.com>
2023-09-25 15:06:11 +0200

    Class uploaded 2

--- src/OrtakKlasor/OrtakClass.java
+++ src/OrtakKlasor/OrtakClass.java
@@ -1,4 +1,17 @@
 package OrtakKlasor;
 
 public class OrtakClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Serkan Test 1");
+        System.out.println("Serkan Test 2");
+        System.out.println("Serkan Test 3");
+        System.out.println("Serkan Test 4");
+        System.out.println("Serkan Test 5");
+        System.out.println("Serkan Test 6");
+        System.out.println("Serkan Test 7");
+        System.out.println("Serkan Test 8");
+        System.out.println("Serkan Test 9");
+    }
 }
--- src/OrtakKlasor/Serkan.java
+++ src/OrtakKlasor/Serkan.java
@@ -5,5 +5,11 @@ public class Serkan {
     public static void main(String[] args) {
 
         System.out.println("Serkan Cüteli");
+        System.out.println("Serkan Cüteli");
+        System.out.println("Serkan Cüteli");
+        System.out.println("Serkan Cüteli");
+        System.out.println("Serkan Cüteli");
+        System.out.println("Serkan Cüteli");
+        System.out.println("Serkan Cüteli");
     }
 }
