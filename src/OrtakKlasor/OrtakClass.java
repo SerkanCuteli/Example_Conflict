da3cf6147dc09861a7845b7183a146018b1e4a50
SerkanCuteli <serkancuteli@gmail.com>
2023-09-25 14:28:22 +0200

    Class uploaded

---
+++ src/OrtakKlasor/OrtakClass.java
@@ -0,0 +1,4 @@
+package OrtakKlasor;
+
+public class OrtakClass {
+}
---
+++ src/OrtakKlasor/Serkan.java
@@ -0,0 +1,9 @@
+package OrtakKlasor;
+
+public class Serkan {
+
+    public static void main(String[] args) {
+
+        System.out.println("Serkan Cüteli");
+    }
+}
